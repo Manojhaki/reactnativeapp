76a93f5659fb47e222508255ce57b458c677c0e2
Manoj Pun <manoj.haki@gmail.com>
2021-07-25 09:59:04 -0400

    so far

---
+++ components/AboutComponent.js
@@ -0,0 +1,31 @@
+import { Component } from "react"
+import { ScrollView } from "react-native";
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+
+    }
+
+
+    static navigationOptions = {
+        title: 'About Us'
+    }
+
+
+    render() {
+
+        return (
+            <ScrollView>
+
+
+            </ScrollView>
+
+        );
+    }
+}
+
+export default About;
