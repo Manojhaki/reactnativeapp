91bac2b46c5607bf495372a30943a4441296ccd6
Manoj Pun <manoj.haki@gmail.com>
2021-08-04 09:54:20 -0400

    animatable

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -7,6 +7,7 @@ import { baseUrl } from '../shared/baseUrl';
 import { View, FlatList, Text } from 'react-native';
 import Loading from './LoadingComponent';
 
+import * as Animatable from 'react-native-animatable';
 
 
 const mapStateToProps = state => {
@@ -26,14 +27,19 @@ class Directory extends Component {
         const { navigate } = this.props.navigation;
         const renderDirectoryItem = ({ item }) => {
             return (
-                <Tile
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    imageSrc={{ uri: baseUrl + item.image }}
+                <Animatable.View animation='fadeInRightBig' duration={2000}>
 
-                />
+                    <Tile
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                        imageSrc={{ uri: baseUrl + item.image }}
+
+
+                    />
+                </Animatable.View>
+
 
             );
         }
